a0e562c4beb09d3279220243db9981a41f8556c4
Roman Lihatskiy <rlihatskiy@determine.com>
2024-09-04 21:22:08 +0200

    random chain component select

--- app/chat/llms/chatopenai.py
+++ app/chat/llms/chatopenai.py
@@ -1,5 +1,5 @@
 from langchain.chat_models import ChatOpenAI
 
 
-def build_llm(chat_args):
-    return ChatOpenAI(streaming=chat_args.streaming)
+def build_llm(chat_args, model_name='gpt-3.5-turbo'):
+    return ChatOpenAI(streaming=chat_args.streaming, model_name=model_name)
