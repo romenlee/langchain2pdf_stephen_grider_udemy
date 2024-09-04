35014b3262ae9f8721edf2c8a1d450c26799b725
Roman Lihatskiy <rlihatskiy@determine.com>
2024-09-04 19:12:50 +0200

    streaming

--- app/chat/chat.py
+++ app/chat/chat.py
@@ -3,6 +3,8 @@ from app.chat.models import ChatArgs
 from app.chat.vector_stores.pinecone import build_retriever
 from app.chat.llms.chatopenai import build_llm
 from app.chat.memories.sql_memory import build_memory
+from app.chat.chains.retrieval import StreamingConversationalRetrievalChain
+from langchain.chat_models import ChatOpenAI
 
 
 def build_chat(chat_args: ChatArgs):
@@ -19,7 +21,16 @@ def build_chat(chat_args: ChatArgs):
 
     retriever = build_retriever(chat_args)
     llm = build_llm(chat_args)
+    condense_question_llm = ChatOpenAI(streaming=False)
     memory = build_memory(chat_args)
+    # streaming
+    return StreamingConversationalRetrievalChain.from_llm(
+        llm=llm,
+        condense_question_llm=condense_question_llm,
+        memory=memory,
+        retriever=retriever
+    )
+    # streaming end
 
     return ConversationalRetrievalChain.from_llm(
         llm=llm,
